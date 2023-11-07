f8b30702abf065f6204966e367be3ae09a1c8e4b
Manish-Sharma09 <manishsharma6501@gmail.com>
2023-11-07 15:31:52 +0530

    Fix line endings

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,20 +1,56 @@
-import React from 'react'
-import './ImageGenerator.css'
-import default_image from '../Assets/default_image.svg'
+// ImageGenerator.jsx
+import React, { useState } from 'react';
+import './ImageGenerator.css';
+import default_image from '../Assets/default_image.svg';
+import apiKey from '../config'; // Update the path accordingly
 
 const ImageGenerator = () => {
+  const [generatedImage, setGeneratedImage] = useState(null);
+  const [description, setDescription] = useState('');
+
+  const generateImage = async () => {
+    try {
+      const response = await fetch('https://api.openai.com/v1/images/generations', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({ description }),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setGeneratedImage(result.image_url);
+      } else {
+        // Log error details
+        console.error(`Error: ${response.status} - ${response.statusText}`);
+        const errorResult = await response.json();
+        console.error('Error details:', errorResult);
+      }
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+  };
+
   return (
     <div className='ai-image-generator'>
       <div className='header'>Ai Image <span>Generator</span></div>
       <div className='img-loading'>
-        <div className='image'><img src={default_image} /></div>
+        <div className='image'><img src={generatedImage || default_image} alt="Generated Image" /></div>
       </div>
       <div className='seacrh-box'>
-        <input type='text' className='search-input'placeholder='Describe what you want'/>
-        <div className='generate-btn'>Generate</div>
+        <input
+          type='text'
+          className='search-input'
+          placeholder='Describe what you want'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <div className='generate-btn' onClick={generateImage}>Generate</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageGenerator
+export default ImageGenerator;
---
+++ src/Components/config.js
@@ -0,0 +1,3 @@
+// config.js
+const apiKey = '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'; // Replace with your actual OpenAI API key
+export default apiKey;
